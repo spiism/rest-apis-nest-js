29cf9a1bd00ed84a3493bbfdf2d764ebd9ec5534
michael <spiism@gmail.com>
2024-11-24 12:49:08 +1300

    create mongo db connection Get endpoint books Post endpoint books

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as process from 'node:process';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    BookModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BookService } from './book.service';
+import { Book } from './schemas/book.schema';
 
-@Controller('book')
-export class BookController {}
+@Controller('books')
+export class BookController {
+  constructor(private bookService: BookService) {}
+
+  @Get()
+  async getAllBooks(): Promise<Book[]> {
+    return this.bookService.findAll();
+  }
+
+  @Post('new')
+  async createBook(
+    @Body()
+    book,
+  ): Promise<Book> {
+    return this.bookService.create(book);
+  }
+}
