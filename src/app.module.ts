c78307d25b957f6ed4e0cd4d0777d2f2fad3fb20
michael <spiism@gmail.com>
2024-11-27 14:59:29 +1300

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 import * as process from 'node:process';
 
 @Module({
@@ -11,6 +12,7 @@ import * as process from 'node:process';
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
     MongooseModule.forRoot(process.env.DB_URI),
     BookModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
