a85766e4dd7e67749a7077dd44cc677285e51247
michael <spiism@gmail.com>
2025-06-08 11:09:33 +1200

    update signup route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
+// import { Roles } from './decorators/roles.decorator';
+// import { Role } from './enums/role.enum';
+// import { AuthGuard } from '@nestjs/passport';
+// import { RolesGuard } from './guards/roles.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
+  // //Should be only admin user can create new user
+  // @Roles(Role.Admin)
+  // @UseGuards(AuthGuard(), RolesGuard)
   signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
     return this.authService.signUp(signUpDto);
   }
