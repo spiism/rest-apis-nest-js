7e3bd7e8e32ea0013e5e727fa56e323d83c29315
michael <spiism@gmail.com>
2025-06-08 12:51:23 +1200

    add Helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('My API')
